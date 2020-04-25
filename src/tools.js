3228ca0613e9e093f5739274a52ca7e7e571becb
Taylor Campbell <taylor@manythingsblue.com>
2020-04-25 11:34:54 -0400

    fix typo

--- src/tools.js
+++ src/tools.js
@@ -1,12 +1,12 @@
 export default {
-  Circle: 'circle',
-  Line: 'line',
-  Arrow: 'arrow',
-  Pencil: 'pencil',
-  Rectangle: 'rectangle',
-  RectangleLabel: 'rectangle-label',
-  Select: 'select',
-  Polygon: 'polygon'
-  Pan: 'pan',
-  DefaultTool: 'default-tool',
-}
+  Circle: "circle",
+  Line: "line",
+  Arrow: "arrow",
+  Pencil: "pencil",
+  Rectangle: "rectangle",
+  RectangleLabel: "rectangle-label",
+  Select: "select",
+  Polygon: "polygon",
+  Pan: "pan",
+  DefaultTool: "default-tool",
+};
